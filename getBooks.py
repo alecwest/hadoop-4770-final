737f233064663445956e048cd088d4eac4501876
Alec West <alecwest38@gmail.com>
2019-04-16 11:15:18 -0500

    Update getbooks to get all genres

--- getBooks.py
+++ getBooks.py
@@ -1,11 +1,23 @@
+import os
 import requests
 import re
 
-location = 'http://www.textfiles.com/etext/FICTION/'
-request = requests.get(location)
-urls = re.sub(r'.*A HREF="(\S+)".*', location + r'\1', request.text)
-urls = re.sub(r'\n?<.*\n?', r'', urls).split('\n')
-for url in urls:
-    print(url)
-    r = requests.get(url)
-    open(re.sub(r'([^\.]+)(\.txt)?', r'/path/to/word-counter/input/' + r'\1.txt', url.split('/')[-1], 1), 'wb').write(r.content) # CHANGE THE PATH HERE
+genres = ['FICTION/', 'NONFICTION/', 'MODERN/', 'REFERENCE/']
+location = 'http://www.textfiles.com/etext/'
+base_path = './input/'
+
+for genre in genres:
+    genre_location = location + genre
+    request = requests.get(genre_location)
+    urls = re.sub(r'.*A HREF="(\S+)".*', genre_location + r'\1', request.text)
+    urls = re.sub(r'\n?<.*\n?', r'', urls).split('\n')
+    for url in urls:
+        file_path = re.sub(r'([^\.]+)(\.txt)?', base_path + r'\1.txt', url.split('/')[-1], 1)
+        if not os.path.isfile(file_path):
+            print(url)
+            r = requests.get(url)
+            if r.status_code == 200:
+                open(file_path, 'wb').write(r.content)
+            else:
+                print('could not get url ' + url)
+
