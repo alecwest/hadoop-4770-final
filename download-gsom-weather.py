0823a3a37506459abcc5ea1d0bfb4c84e426025f
Alec West <alecwest38@gmail.com>
2019-04-28 22:29:11 -0500

    update download-gsom-weather to combine csvs right after each tarfile download

--- download-gsom-weather.py
+++ download-gsom-weather.py
@@ -1,27 +1,35 @@
 import os
+import glob
+import pandas as pd
 import requests
 import tarfile
 
 location = 'https://www.ncei.noaa.gov/data/global-summary-of-the-day/archive/'
 years = [i for i in range(1929, 2020)]
-base_path = './weather-input/'
+os.chdir('weather-input')
 
 for year in years:
     file_name = str(year) + ".tar.gz"
-    file_path = base_path + file_name
-    tar_location = location + file_path
+    tar_location = location + file_name
     url = location + file_name
-    if not os.path.isfile(file_path):
+    if not os.path.isfile(file_name):
         print(url)
         response = requests.get(url)
         if response.status_code == 200:
-            with open(file_path, 'wb') as f:
+            with open(file_name, 'wb') as f:
                 f.write(response.content)
-            print('extracting ' + file_path)
-            tf = tarfile.open(file_path)
-            tf.extractall(path=base_path)
+            print('extracting ' + file_name)
+            tf = tarfile.open(file_name)
+            tf.extractall()
+            # combine files into one csv for the year
+            filenames = tf.getnames()
+            combined_csv = pd.concat([pd.read_csv(f) for f in filenames])
+            print('combined {} files'.format(len(filenames)))
+            combined_csv.to_csv('{}.csv'.format(year), index=False, encoding='utf-8')
             tf.close()
-            os.remove(file_path)
+            # delete all files
+            os.remove(file_name)
+            for f in filenames:
+                os.remove(f)
         else:
             print('could not get url ' + url)
-
